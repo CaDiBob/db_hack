b7e40859d36cdbefc0afe1cc9f381f86a0de0e6c
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-02-23 18:00:13 +0900

    Update fix_marks.py
    pep8 корректировка

--- fix_marks.py
+++ fix_marks.py
@@ -1,5 +1,6 @@
 from datacenter.models import Schoolkid, Mark
 
+
 def fix_marks(name):
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains=name)
@@ -8,7 +9,7 @@ def fix_marks(name):
     except Schoolkid.MultipleObjectsReturned:
         return 'Нужно ввести Фамилию и Имя'
 
-    marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2,3])
+    marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
     for mark in marks:
         mark.points = 5
         mark.save()
